442878eed42a63a9a293b599cb6504fe83d3b257
Rami Grossman <ramigg@gmail.com>
2012-02-13 18:23:59 +0200

    added additional check in sound notification sending

--- lib/apn_on_rails/app/models/apn/group_notification.rb
+++ lib/apn_on_rails/app/models/apn/group_notification.rb
@@ -45,7 +45,7 @@ class APN::GroupNotification < APN::Base
     result['aps']['alert'] = self.alert if self.alert
     result['aps']['badge'] = self.badge.to_i if self.badge
     if self.sound
-      result['aps']['sound'] = self.sound if self.sound.is_a? String
+      result['aps']['sound'] = self.sound if self.sound.is_a?(String) && self.sound.strip.present?
       result['aps']['sound'] = "1.aiff" if self.sound.is_a?(TrueClass)
     end
     if self.custom_properties
